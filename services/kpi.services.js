307db48f1633546aa319a6189c030a71adfee6b3
MTT <tuantran.pltn@gmail.com>
2022-01-20 16:35:58 +0700

    update

--- services/kpi.services.js
+++ services/kpi.services.js
@@ -1,6 +1,6 @@
 import { getRequest, putRequest } from "utils/request";
 
-const serviceURL = `http://localhost:1337`;
+const serviceURL = `https://uiux-20211.vercel.app`;
 
 export async function getKPIs() {
   const url = serviceURL + `/api/kpis?populate=*&sort=fullName:asc`;
